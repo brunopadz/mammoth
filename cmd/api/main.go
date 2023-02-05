253e5f80c2224ef85db52e0c73f8ef71948214dc
brunopadz <bpadz@protonmail.com>
2023-02-05 18:07:03 -0300

    Initial api idea

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,21 @@
-package api
+package main
+
+import (
+	"github.com/brunopadz/mammoth/cmd/api/routes"
+	"github.com/brunopadz/mammoth/util/log"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	//r := redisConn()
+
+	a := fiber.New()
+	a.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Send([]byte("mammoth api server"))
+	})
+	routes.UsersRouter(a)
+	err := a.Listen(":8080")
+	if err != nil {
+		log.Fatal("Could not listen")
+	}
+}
--- cmd/api/routes/routes.go
+++ cmd/api/routes/routes.go
@@ -1,7 +1,50 @@
 package routes
 
-import "github.com/go-redis/redis/v9"
+import (
+	"context"
+	"github.com/brunopadz/mammoth/util/log"
+	"github.com/go-redis/redis/v9"
+	"github.com/gofiber/fiber/v2"
+	"net/http"
+)
 
-func redisConn() {
-	redis.NewClient()
+type User struct {
+	User string
+}
+
+func UsersRouter(app fiber.Router) {
+
+	ctx := context.Background()
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		r := redisConn()
+		d, err := r.SMembers(context.Background(), "users").Result()
+		if err != nil {
+			log.Fatal("aaa")
+		}
+		return c.JSON(d)
+	})
+	app.Put("/users", func(c *fiber.Ctx) error {
+		var d User
+		r := redisConn()
+		err := c.BodyParser(&d)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+		}
+		// Check user exists if not, add user
+		r.SAdd(ctx, "users", &d)
+
+		// uncomment below
+		//return c.Send([]byte("to-do"))
+	})
+	app.Delete("/users", func(c *fiber.Ctx) error {
+		return c.Send([]byte("to-do"))
+	})
+}
+
+func redisConn() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
 }
